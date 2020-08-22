063781d5187d8efe76b2f85f0931cc4993bd6e1c
mukesh kumar mahto <mukesh.7755@gmail.com>
2020-08-22 05:38:49 +0300

    fibonacci series iteration and recursive approach

---
+++ fibo.py
@@ -0,0 +1,24 @@
+def fiboIte(n):
+    prevnum=0
+    currentnum=1
+    for i in range(1,n):
+        prevprevnum = prevnum
+        prevnum = currentnum
+        currentnum = prevnum + prevprevnum
+    return currentnum
+
+
+
+def fiboRec(n):
+    if n==0:
+        return 0
+    elif(n==1):
+        return 1
+    else:
+        return fiboRec(n-1) + fiboRec(n-2)
+
+
+if __name__ == "__main__":
+    num = int(input("enter a number"))
+    print(f"using recursion the value of fib({num}) is {fiboRec(num)}")
+    print(f"using recursion the value of fib({num}) is {fiboIte(num)}")
